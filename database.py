ca6617f04abc12c1d1874d71b51bc78bb8ce4348
pveeraprince002 <p.veeraprince002@gmail.com>
2025-02-25 22:35:55 +0530

    Modifited the database

--- database.py
+++ database.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+#database connection 
+
 sqlPath = 'database.db'
 sql_url = f"sqlite:///./{sqlPath}"
 engine = create_engine(sql_url,connect_args={"check_same_thread": False})
